f078aede8c1b68c4b6b53c429182de2971d750b5
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-07-01 11:02:23 +0300

    http

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const formEl = document.getElementById('search-form');
 const inputEl = document.querySelector('input[name="searchQuery"]');
 let page = 0;
 
-const BASE_URL = 'https://pixabay.com/api/';
+const BASE_URL = 'http://pixabay.com/api/';
 const API_KEY = '37960657-3cfa1dcb3808e6e9b644b5e90'
 
 loadMoreBtnEl.style.display="none";
